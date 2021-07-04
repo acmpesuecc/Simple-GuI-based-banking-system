cffd129cadf1e1eaba714bfa5b3f154a929cf3bf
Cdsclaudy <84378737+Cdsclaudy@users.noreply.github.com>
2021-07-04 19:56:23 +0530

    Really sorry, just one last change

--- MiniProject1/encrypt.h
+++ MiniProject1/encrypt.h
@@ -11,41 +11,41 @@ void encrypt(char *a)
     char n[40];
     int l=strlen(a);
     int count=0;
-    for(int i=0;i<l;i++)
+    for(int i=0;i<2*l;i+=2)
     {
         if(i%4==0)
         {
-            n[i]=(a[i]+A[count]);
+            n[i]=(a[i/2]+A[count]);
             
         }
         else if(i%3==0)
         {
-            n[i]=(a[i]+C[count]);
+            n[i]=(a[i/2]+C[count]);
             
         }
         else if(i%2==0)
         {
-            n[i]=(a[i]+B[count]);
+            n[i]=(a[i/2]+B[count]);
             
         }
         else
         {
-            n[i]=(a[i]+D[count]);
+            n[i]=(a[i/2]+D[count]);
             
         }
-        /*if(n[i]=='\n')
+        if(n[i]=='\n')
         n[i]='!';
         else if(n[i]=='\t')
         n[i]='*';
         else if(n[i]==',')
-        n[i]='#';*/
+        n[i]='#';
         count++;
         if(count==5)
         count=0;
     }
-    n[l]=';'-2*l;
+    //n[l]=';'-2*l;
     count=0;
-    for(int i=l+1;i<39;i++)
+    for(int i=1;i<39;i+=2)
     {
         if(i%4==0)
         {
@@ -79,6 +79,7 @@ void encrypt(char *a)
         if(count==5)
         count=0;
     }
+    n[l]=';'-2*l;
     n[39]='\0';
     //printf("%s",n);
     strcpy(a,n);
@@ -96,23 +97,23 @@ void decrypt(char *a)
     {
         if((a[i]+2*i)==';')
         {
-            l=i;
+            l=i/2;
             //printf("%d",l);
             break;
         }
     }
     char n[l];
     int count=0;
-    for(int i=0;i<l;i++)
+    for(int i=0;i<2*l;i+=2)
     {
-        if(a[i]=='!')
-        a[i]='\n';
-        else if(a[i]=='*')
-        a[i]='\t';
-        else if(a[i]=='#')
-        a[i]=',';
-        else if(n[i]=='$')
-        n[i]=' ';
+        if(a[i/2]=='!')
+        a[i/2]='\n';
+        else if(a[i/2]=='*')
+        a[i/2]='\t';
+        else if(a[i/2]=='#')
+        a[i/2]=',';
+        else if(n[i/2]=='$')
+        n[i/2]=' ';
         if(i%4==0)
         {
             n[i]=(a[i]-A[count]);
@@ -141,4 +142,3 @@ void decrypt(char *a)
     //printf("%s",n);
     strcpy(a,n);
 }
-
