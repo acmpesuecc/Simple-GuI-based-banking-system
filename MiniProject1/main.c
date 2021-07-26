d5d40f64345b9b4b2a8977f465468d83434f62ac
Ashutosh <72194162+Theorybegetter@users.noreply.github.com>
2021-07-26 12:21:40 +0530

    Update main.c
    encryptionless code

--- MiniProject1/main.c
+++ MiniProject1/main.c
@@ -1,145 +1,120 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<time.h>
 
-void deposit(char *g)
+int size=0;
+char custom[]="";
+char A[]="LOREM";
+char B[]="IPSUM";
+char C[]="CONSE";
+char D[]="CRITO";
+
+struct customer {
+    char userName[40];
+    char password[40];
+    char accNo[40];//16bits long
+    char IFSCcode[40];//10bits long
+    float balance;
+    char upiId[40];//10bits long
+    int upiPass;//6digits
+}s[100];
+
+void encrypt(char *a)
 {
-    int n=1;
-    float cash,depocash;
-    printf("enter the amount of cash to be deposited\n");
-    scanf("%f",&depocash);
-    char user[20];
-    strcpy(user,g);
-    char token[1024];
-    
-    FILE* p = fopen("user.csv", "r");
-    FILE* s=fopen("check.csv","w");
-    if(!p)printf("File empty\n");
-    if(!s)printf("File empty\n");
-    else 
+    char n[40];
+    int l=strlen(a);
+    int count=0;
+    for(int i=0;i<l;i++)
     {
-        char buffer[1024];
-        int row = 0,column=0;
-        char cashstr[20],cashstr1[20];
-        fputs("Username,password,balance\n",s);
-        while (fgets(buffer,1024, p)) 
-        {
-            column = 0;
-            row++;
-            if (row == 1)continue;
-            char* value = strtok(buffer, ",");
-            if(!strcmp(user,value))
-            {fputs(value,s);
-            fputs(",",s);
-            value = strtok(NULL, ",");
-            fputs(value,s);
-            fputs(",",s);
-            value = strtok(NULL, ",");
-            cash=atof(value);
-            cash+=depocash;
-            gcvt(cash,15,cashstr);
-            for(int e=0;e<strlen(cashstr)-1;e++)
-            {cashstr1[e]=cashstr[e];}
-            fputs(cashstr1,s);
-            fputs("\n",s);}
-            else
-            {fputs(value,s);
-            fputs(",",s);
-            value = strtok(NULL, ",");
-            fputs(value,s);
-            fputs(",",s);
-            value = strtok(NULL, ",");
-            fputs(value,s);
-            }
-        }
-        fclose(p);
-        fclose(s);
-        FILE* p = fopen("user.csv", "w+");
-        FILE* s=fopen("check.csv","r");
-        while(fgets(buffer,1024,s))
-        {fputs(buffer,p);}
-        fclose(p);
-        fclose(s);
-        printf("tasks performed successfully");
+        if(i%4==0)
+        {n[i]=(a[i]+A[count]);}
+        else if(i%3==0)
+        {n[i]=(a[i]+C[count]);}
+        else if(i%2==0)
+        {n[i]=(a[i]+B[count]);}
+        else
+        {n[i]=(a[i]+D[count]);}
+        count++;
+        if(count==5)
+        count=0;
     }
+    n[l]=';'-2*l;
+    count=0;
+    for(int i=l+1;i<39;i++)
+    {
+        if(i%4==0)
+        {n[i]=(B[count]+A[count]);}
+        else if(i%3==0)
+        {n[i]=(B[count]+C[count]);}
+        else if(i%2==0)
+        {n[i]=(D[count]+A[count]);}
+        else
+        {n[i]=(B[count]+D[count]);}
+        if(n[i]=='\n')
+        n[i]='!';
+        else if(n[i]=='\t')
+        n[i]='"';
+        else if(n[i]==',')
+        n[i]='#';
+        else if(n[i]==' ')
+        n[i]='$';
+        count++;
+        if(count==5)
+        count=0;
+    }
+    n[39]='\0';
+    strcpy(a,n);
 }
 
-void withdraw(char *g)
+void decrypt(char *a)
 {
-    int n=1;
-    float cash,withcash;
-    printf("enter the amount of cash to be withdrawn\n");
-    scanf("%f",&withcash);
-    char user[20];
-    strcpy(user,g);
-    char token[1024];
-    
-    FILE* p = fopen("user.csv", "r");
-    FILE* s=fopen("check.csv","w");
-    if(!p)printf("File empty\n");
-    if(!s)printf("File empty\n");
-    else 
+    int l;
+    for(int i=0;i<40;i++)
     {
-        char buffer[1024];
-        int row = 0,column=0;
-        char cashstr[20],cashstr1[20];
-        fputs("Username,password,balance\n",s);
-        while (fgets(buffer,1024, p)) 
-        {
-            column = 0;
-            row++;
-            if (row == 1)continue;
-            char* value = strtok(buffer, ",");
-            if(!strcmp(user,value))
-            {fputs(value,s);
-            fputs(",",s);
-            value = strtok(NULL, ",");
-            fputs(value,s);
-            fputs(",",s);
-            value = strtok(NULL, ",");
-            cash=atof(value);
-            cash-=withcash;
-            gcvt(cash,15,cashstr);
-            for(int e=0;e<strlen(cashstr)-1;e++)
-            {cashstr1[e]=cashstr[e];}
-            fputs(cashstr1,s);
-            fputs("\n",s);}
-            else
-            {fputs(value,s);
-            fputs(",",s);
-            value = strtok(NULL, ",");
-            fputs(value,s);
-            fputs(",",s);
-            value = strtok(NULL, ",");
-            fputs(value,s);
-            }
-        }
-        fclose(p);
-        fclose(s);
-        FILE* p = fopen("user.csv", "w+");
-        FILE* s=fopen("check.csv","r");
-        while(fgets(buffer,1024,s))
-        {fputs(buffer,p);}
-        fclose(p);
-        fclose(s);
-        printf("tasks performed successfully");
+        if((a[i]+2*i)==';')
+        {l=i;
+        break;}
     }
+    char n[l];
+    int count=0;
+    for(int i=0;i<l;i++)
+    {
+        if(a[i]=='!')
+        a[i]='\n';
+        else if(a[i]=='*')
+        a[i]='\t';
+        else if(a[i]=='#')
+        a[i]=',';
+        else if(n[i]=='$')
+        n[i]=' ';
+        if(i%4==0)
+        {n[i]=(a[i]-A[count]);}
+        else if(i%3==0)
+        {n[i]=(a[i]-C[count]);}
+        else if(i%2==0)
+        {n[i]=(a[i]-B[count]);}
+        else
+        {n[i]=(a[i]-D[count]);}
+        count++;
+        if(count==5)
+        count=0;
+    }
+    n[l]='\0';
+    strcpy(a,n);
 }
 
-int balance(char *g)
+void getFile(struct customer c[])
 {
-    FILE* fp = fopen("user.csv", "r");
-  
+    FILE* fp = fopen("exceptional.csv", "r");
+    int i=0;
     if (!fp)
-    {
         printf("File is empty!!\n");
-        return 0;
-    }
     else 
     {
-        char buffer[1024];
+        char buffer[256];
         int row = 0,column=0;
-        while (fgets(buffer,1024, fp)) 
+        while (fgets(buffer,256, fp)) 
         {
             column = 0;
             row++;
@@ -149,187 +124,337 @@ int balance(char *g)
   
             while (value) 
             {
-                if (column == 0 && strcmp(g,value)==0)
-                {
-                    value = strtok(NULL, ",");
-                    value = strtok(NULL, ",");
-                    printf("%s",value);
-                    return 0;
-                }
- 
+                strcpy(s[i].userName,value);
                 value = strtok(NULL, ",");
-
+                strcpy(s[i].password,value);
+                value = strtok(NULL, ",");
+                strcpy(s[i].accNo,value);
+                value = strtok(NULL, ",");
+                strcpy(s[i].IFSCcode,value);
+                value = strtok(NULL, ",");
+                s[i].balance = strtof(value,NULL);
+                value = strtok(NULL, ",");
+                strcpy(s[i].upiId,value);
+                value = strtok(NULL, ",");
+                s[i].upiPass = atoi(value);
+                value = strtok(NULL, ",");
+                i++;
             }
         }
         fclose(fp);
     }
+    size=i;
+}
+
+void putFile(struct customer c[])
+{
+    FILE* fp = fopen("exceptional.csv", "w");
+    int i=0;
+    fputs("Username,Password,Accno,IFSCcode,Balance,UPIid,UPIpass\n",fp);
+    while(i<size)
+    {
+        fputs(s[i].userName,fp);
+        fputc(',',fp);
+        fputs(s[i].password,fp);
+        fputc(',',fp);
+        fputs(s[i].accNo,fp);
+        fputc(',',fp);
+        fputs(s[i].IFSCcode,fp);
+        fputc(',',fp);
+        char text[20];
+        sprintf(text, "%.2f", s[i].balance); 
+        fputs(text,fp);
+        fputc(',',fp);
+        fputs(s[i].upiId,fp);
+        fputc(',',fp);
+        sprintf(text, "%d", s[i].upiPass); 
+        fputs(text,fp);
+        fputs("\n",fp);
+        i++;
+    }
+    fclose(fp);
+}
+
+void IFSCGenerator(char *s)
+{
+    srand(time(0));
+    char str[15]="PESU";
+    for(int i=4;i<10;i++)
+    {
+        int n = rand()%10;
+        char ch = '0'+n;
+        str[i]=ch;
+    }
+    strcpy(s,str);
+}
+
+void accGenerator(char *s)
+{
+    srand(time(0));
+    char str[17]="";
+    for(int i=0;i<16;i++)
+    {
+        int n = rand()%10;
+        char ch = '0'+n;
+        str[i]=ch;
+    }
+    strcpy(s,str);
 }
 
 int check_user(char user[])
 {
-    FILE* fp = fopen("user.csv", "r");
-    if (!fp)printf("File Empty\n");
-    else 
-    {
-        char buffer[1024];
-        int row = 0,column=0;
-        while (fgets(buffer,1024, fp)) 
-        {
-            column = 0;
-            row++;
-            if (row == 1)continue;
-            char* value = strtok(buffer, ", ");
-            if(!strcmp(user,value))
-            return 1;
-        }
-        fclose(fp);
-    }
+    for(int i=0;i<size;i++)
+        if(!strcmp(s[i].userName,user))return 1;
     return 0;
+}
+
+void createUPI(int i)
+{
+    char str[40];
+    printf("The upcoming information will be collected to create UPI: \n");
+    printf("Enter your 10 digit mobile number: ");
+    scanf("%s",&str);
+    str[10]='@';
+    str[11]='p';
+    str[12]='e';
+    str[13]='s';
+    
+    strcpy(s[i].upiId,str);
+    pin_start:
+    printf("Enter a 6 digit UPI PIN(avoid using 0 as 1st digit): ");
+    scanf("%d",&s[i].upiPass);
+    if(s[i].upiPass>=100000 && s[i].upiPass<=999999)
+    {
+        printf("Your UPI id was created successfully!\n");
+        return;
+    }
+    else goto pin_start;
+}
+
+void deposit(char *user)
+{
+    float cash;
+    printf("\nEnter the cash to be deposited: ");
+    scanf("%f",&cash);
+    getFile(s);
+    for(int i=0;i<size;i++)
+    {
+        if(strcmp(s[i].userName,user)=='0'||strcmp(s[i].userName,user)==0)
+        {s[i].balance+=cash;}
+    }
+    putFile(s);
+    printf("Deposit successfull\n");
+}
+
+void withdraw(char *user)
+{
+    float cash;
+    printf("\nEnter the cash to be withdrawn: ");
+    scanf("%f",&cash);
+    getFile(s);
+    for(int i=0;i<size;i++)
+    {
+        if(strcmp(s[i].userName,user)=='0'||strcmp(s[i].userName,user)==0)
+        {s[i].balance-=cash;}
+    }
+    putFile(s);
+    printf("Withdraw successfull\n");
+}
+
+void balance(char *user)
+{
+    getFile(s);
+    for(int i=0;i<size;i++)
+        if(strcmp(s[i].userName,user)=='0'||strcmp(s[i].userName,user)==0)
+            printf("Your Balance = %.2f\n",s[i].balance);
+    putFile(s);
+}
+
+void transfer(char *user)
+{
+    char reciever[15],reci[15];
+    int i,checkpass,u,flag=0;
+    printf("\nEnter your UPIpass to continue: ");
+    abc:
+    scanf("%d",&checkpass);
+    getFile(s);
+    for(i=0;i<size;i++)
+    {
+        if(strcmp(s[i].userName,user)=='0'||strcmp(s[i].userName,user)==0)
+        {   
+            if(s[i].upiPass==checkpass)
+            {
+                printf("UPIpass matched\n");
+                u=i;
+            }
+            else
+            {
+                printf("Wrong UPIpass,try again\n");
+                goto abc;
+            }
+        }
+    }
+    printf("Enter a reciever UPIid\n");
+    xyz:
+    scanf("%s",&reciever);
+    getFile(s);
+    float cash;
+    for(int i=0;i<size;i++)
+    {
+        if(strcmp(s[i].upiId,reciever)=='0'||strcmp(s[i].upiId,reciever)==0)
+        {
+            printf("Enter the amount of cash to be transferred: ");
+            scanf("%f",&cash);
+            s[u].balance-=cash;
+            s[i].balance+=cash;
+            flag=1;
+            putFile(s);}
+    }
+    if(flag!=1)
+    {
+        printf("Enter the correct reciever upiID! ");
+        goto xyz;
+    }
+    printf("Money transfer successfull!\n");
+}
+
+void quit()
+{
+    printf("Thanks for visiting");
 }
 
 void signup()
 {
-    char username[32],password[12],repass[12];
-    int balance;
+    size++;
+    int i=size-1;
+    char username[32],password[50],repass[50];
     user_start:
     printf("\n---------Welcome to the Sign up page--------\nEnter your username: ");
     scanf("%s",&username);
     if(check_user(username))
     {
-        printf("Username has already been taken!");
+        printf("Username has already been taken!\n");
         goto user_start;
     }
     pass_start:
-    printf("Enter your password:");
+    printf("Enter your password: ");
     scanf("%s",&password);
     if(strlen(password)<8)
     {
         printf("Enter a password greater than 8 characters!\n");
         goto pass_start;
     }
-    printf("Renter your password:");
+    printf("Renter your password: ");
     scanf("%s",&repass);
     if(strcmp(repass,password)==0)
     {
-        FILE* fp = fopen("user.csv", "a+");
-        if (!fp) 
-        {
-            printf("Can't open file\n");
-        }
-        else
-        {
-            printf("Enter bank balance in rupees: ");
-            scanf("%d",&balance);
-            fprintf(fp, "%s,%s,%d\n", username, password,balance);
-            printf("\nNew Account added to record\n");
-        }
+        strcpy(s[i].userName,username);
         
-        fclose(fp);
+        strcpy(s[i].password,password);
+        accGenerator(s[i].accNo);
+        
+        IFSCGenerator(s[i].IFSCcode);
+        
+        s[i].balance=0;
+        createUPI(i);
+        putFile(s);
+        printf("You have successfully created a new account!\n");
     }
     else
     {
-        printf("Re-entered password doesn't work, please try again!");
+        printf("Re-entered password doesn't work, please try again!\n");
         goto pass_start;
     }
 }
 
 int login_checker(char username[],char pass[])
 {
-    FILE* fp = fopen("user.csv", "r");
-  
-    if (!fp)
+    getFile(s);
+    for(int i=0;i<size;i++)
     {
-        printf("File is empty!!\n");
-        return 0;
-    }
-    else 
-    {
-        char buffer[1024];
-        int row = 0,column=0;
-  
-        while (fgets(buffer,1024, fp)) 
-        {
-            column = 0;
-            row++;
-            if (row == 1)continue;
-  
-            char* value = strtok(buffer, ",");
-  
-            while (value) 
-            {
-                if (column == 0 && strcmp(username,value)==0)
-                {
-                    value = strtok(NULL, ",");
-                    strcpy(pass,value);
-                    return 1;
-                }
- 
-                value = strtok(NULL, ",");
-
-            }
+        if(strcmp(s[i].userName,username)=='0'||strcmp(s[i].userName,username)==0){
+            printf("Username found...\nPlease enter password\n");
+            char p[50];
+            strcpy(p,s[i].password);
+            //decrypt(p);
+            strcpy(pass,p);
+            return 1;
         }
-        fclose(fp);
     }
     return 0;
 }
+
 void login()
 {
-    char pass[12],username[20],password[12];
+    int y;
+    char logcheckuser[50],logcheckpass[50],pass[50];
     printf("\n---------Welcome to the login page--------\n");
     user_start:
-    printf("Enter your username:");
-    scanf("%s",&username);
-    if(!login_checker(username,pass))
+    printf("Enter your username: ");
+    scanf("%s",&logcheckuser);
+    if(!login_checker(logcheckuser,pass))
     {
         printf("Enter valid username!\n");
         goto user_start;
     }
     pass_start:
-    printf("Enter your password:");
-    scanf("%s",&password);
-   // printf("%s",pass);
-    if(strcmp(pass,password)=='0')
-    {
-        printf("Enter valid password!\n");
+    printf("Enter your password: ");
+    scanf("%s",&logcheckpass);
+    if(strcmp(pass,logcheckpass)=='0'||strcmp(pass,logcheckpass)==0)
+        printf("logged in\n");
+    else{
+        printf("enter again\n");
         goto pass_start;
     }
-    //printf("Login Successfull!!!!\n");
-    int q;
-    printf("logged in");
-    printf("--redirect to bank--\ntype 1 or 2 or 3 for deposit or withdraw or to check balance respectfully\n");
-    scanf("%d",&q);
-    if(q==1)
-    deposit(username);
-    else if(q==2)
-    withdraw(username);
-    else if(q==3)
-    balance(username);
+    whtodo:
+    printf("\n----Select any of the following operations----\n");
+    printf("(1)Deposit Money\n");
+    printf("(2)Withdraw Money\n");
+    printf("(3)Show Balance\n");
+    printf("(4)Tranfer funda via UPI\n");
+    printf("(5)Exit\n");
+    printf("Enter an option: ");
+    scanf("%d",&y);
+    if(y==1){
+        deposit(logcheckuser);
+        goto whtodo;}
+    else if(y==2){withdraw(logcheckuser);
+        goto whtodo;}
+    else if(y==3){balance(logcheckuser);
+        goto whtodo;}
+    else if(y==4){transfer(logcheckuser);
+    goto whtodo;}
+    else if(y==5)quit();
     else
-    printf("quit successfully");
+    {
+        printf("Enter one of the given values\n");
+        goto whtodo;
+    }
 }
+
 int main()
 {
-    printf("------Welcome to PES's bank simulator------- \n (1)Login \n (2)Sign up\nEnter 1 or 2 respectfully\n");
+    printf("------Welcome to PES's bank simulator------- \n (1)Login \n (2)Sign up\nEnter 1 or 2 respectfully: ");
     int a;
+    getFile(s);
     while(1)
     {
         scanf("%d",&a);
         if(a==1)
         {
-            printf("You will be directed to the login page.....");
+            printf("You will be directed to the login page.....\n");
             login();
             break;
         }
         else if(a==2)
         {
-            printf("You will be directed to the sign up page.....");
+            printf("You will be directed to the sign up page.....\n");
             signup();
+            login();
             break;
         }
         else
-        {
             printf("Enter your choice again as a 1 or 2\n");
-        }
     }
     return 0;
 }
